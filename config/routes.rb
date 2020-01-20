d0752410cf527d89cf8643fce5d9c7209247213c
Casey <caseyrsmith43@gmail.com>
2020-01-20 15:35:58 +0000

    GET route added to display form in erb new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  # Define where rails redirect index
+  root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/contact'
+  resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
