b2c826f259a792bad8acf4c1626945470a5687d8
Casey <caseyrsmith43@gmail.com>
2020-02-03 15:41:42 +0000

    Controller for author destroyed and recteayed

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,3 +1,8 @@
 class Author < ApplicationRecord
 has_many :novels
+# Set the attributes to required
+validates :fname, presence: true
+validates :lname, presence: true
+validates :bio, presence:true
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
+  get 'authors/index'
+  get 'authors/show'
+  get 'authors/new'
+  get 'authors/create'
+  get 'authors/update'
+  get 'authors/edit'
+  get 'authors/destroy'
+  get 'author/index'
+  get 'author/show'
+  get 'author/new'
+  get 'author/create'
+  get 'author/update'
+  get 'author/edit'
+  get 'author/destroy'
   get 'static_pages/home'
   # Define where rails redirect index
   root 'static_pages#home'
   get 'static_pages/about'
   get 'static_pages/contact'
   resources :books
+  resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
